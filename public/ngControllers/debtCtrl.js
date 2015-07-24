f2c4707605fff0395b7d9a18227a0a0ce70572a2
Princeton <collinsprinceton@gmail.com>
2015-07-24 15:37:49 -0700

    almost completed controllers/services?

--- public/ngControllers/debtCtrl.js
+++ public/ngControllers/debtCtrl.js
@@ -2,12 +2,25 @@ var app = angular.module('payback');
 
 app.controller('debtCtrl', function($scope, debtService) {
 
-	$scope.createNewDebt = function(message){
-		debtService.createDebt(message).then(function(res){
-			$scope.
+	$scope.getDebtData = function() {
+		debtservice.getDebts().then(function(response) {
+			$scope.debts = response.data;
 		})
 	}
 
+	$scope.createNewDebt = function(debt) {
+		debtService.createDebt(debt).then(function() {
+			$scope.getDebtData();
+			console.log("Debt created.")
+		})
+	}
+
+	$scope.removeDebt = function(debt) {
+		debtService.removeDebt(debt).then(function() {
+			$scope.getDebtData();
+			console.log("Debt deleted.")
+		})
+	}
 	
 
 })
--- public/ngServices/debtService.js
+++ public/ngServices/debtService.js
@@ -2,14 +2,28 @@ var app = angular.module('payback');
 
 app.service('debtService', function($http) {
 
-	this.createDebt = function() {
+	this.getDebts = function() {
+	 	return $http({
+	 		method: 'GET',
+	 		url: 'https://localhost:1337/payback/debts',
+	 	})
+	 }
+
+	this.createDebt = function(debt) {
 	  	return $http({
 	    method: 'POST',
 	    url: 'http://localhost:1337/payback/debts',
 	    data: {
-	    	
-	    }
+	    	date: Date.now(),
+	    	text: debt
+	  	}
 	  })
 	 }
 
-})
+	 this.removeDebt = function(debt) {
+	 	$http({
+	 		method 'DELETE',
+	 		url: 'http://localhost:1337/payback/debts/:id',
+	 	})
+	 }
+}) //Close service.
