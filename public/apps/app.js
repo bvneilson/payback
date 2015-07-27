83176d7ce0ebe6a48842537e3bbc9c1249ba65e7
kannermichael <michael.kanner4@gmail.com>
2015-07-27 10:11:59 -0600

    updated index, added app.js, and updated mainCtrl, and added passport-local strategy

--- public/apps/app.js
+++ public/apps/app.js
@@ -1,5 +1,34 @@
-var app = angular.module('wwhApp', ['ngRoute', 'ngGrid']);
+var app = angular.module('payback', ['ngRoute']);
 
-app.config(function($routeProvider){
-	$routeProvider
-	
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'view/main.html',
+      // controller: 'mainCtrl'
+    })
+    .when('/dashboard', {
+      templateUrl: 'view/dashboard.html',
+      // controller: 'dashboardCtrl',
+    //   resolve: {
+    //     user: function(dashboardService){
+    //       return dashboardService.getUser().then(function(response){
+    //         console.log(111111, response)
+    //         return response;
+    //       });
+    //     }
+    // }
+    })
+    // .when('/newgoal', {
+    //   templateUrl: 'templates/newgoal.html',
+    //   controller: 'newgoalCtrl'
+    // })
+    // .when('/about', {
+    //   templateUrl: 'templates/about.html'
+    // })
+    // .when('/contact', {
+    //   templateUrl: 'templates/contact.html'
+    // })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
