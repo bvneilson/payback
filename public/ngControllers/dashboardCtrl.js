9a29936d6d16c2351a6a5e29a59e27af082a9b10
Braxton Christensen <braxton.christensen@gmail.com>
2015-07-28 11:11:39 -0600

    updated passport now logsout

--- public/ngControllers/dashboardCtrl.js
+++ public/ngControllers/dashboardCtrl.js
@@ -1,13 +1,14 @@
 var app = angular.module("payback");
 
 app.controller("dashboardCtrl", function($scope, user, dashboardService) {
-    $scope.user = user
-    console.log(2222, $scope.user)
+    $scope.user = user;
+    console.log(2222, $scope.user);
     $scope.getdata = function(get){
         dashboardService.getTheData(get).then(function(res) {
+        	$scope.user = res;
             console.log(res);
-        })
-    }
+        });
+    };
     
 
 });
