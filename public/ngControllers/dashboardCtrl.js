082d9c2d67c8f5c490b3e854a2c5c51acd6a2e18
Braxton Christensen <braxton.christensen@gmail.com>
2015-07-28 11:49:35 -0600

    swiched to bcrypt-nodejs and no errors

--- public/ngControllers/dashboardCtrl.js
+++ public/ngControllers/dashboardCtrl.js
@@ -1,13 +1,13 @@
 var app = angular.module("payback");
 
 app.controller("dashboardCtrl", function($scope, user, dashboardService) {
-    console.log(2222, $scope.user);
+    console.log(2222, $scope.user)
     $scope.getdata = function(get){
         dashboardService.getTheData(get).then(function(res) {
         	$scope.user = res; 
             console.log(res);
-        });
-    };
+        })
+    }
     
 
 });
