e069776f45e6516224a4fb361705101056850e74
Braxton Christensen <braxton.christensen@gmail.com>
2015-08-03 14:36:21 -0600

    bobs work

--- dbModels/User.js
+++ dbModels/User.js
@@ -3,15 +3,10 @@ var bcrypt = require('bcrypt-nodejs');
 
 var userSchema = new mongoose.Schema({
   email: { type: String, required: true },
-  // userName: { type: String, required: true },
   password: { type: String, required: true },
-  // cellPhone: { type: String, required: true },
-  // address: { type: String },
-  // city: { type: String },
-  // state: { type: String },
-  // zip: { type: String },
-  // messages: { type: String}
-  debtId: [ {type:mongoose.Schema.Types.ObjectId, ref: 'Debt', required: true} ]
+  debtId: [ {type:mongoose.Schema.Types.ObjectId, ref: 'Debt', required: true} ],
+  // userName: { type: String },
+  // cellPhone: { type: String }
 });
 
 // methods ======================
--- public/scripts/init.js
+++ public/scripts/init.js
@@ -1,7 +1,8 @@
 // (function($){
-//  $(function(){
-//    $('.button-collapse').sideNav();
-//    $('.parallax').parallax();
-//    $('.modal-trigger').leanModal();
-//  }); // end of document ready
+//   $(function(){
+
+//     $('.button-collapse').sideNav();
+//     $('.parallax').parallax();
+
+//   }); // end of document ready
 // })(jQuery); // end of jQuery name space
