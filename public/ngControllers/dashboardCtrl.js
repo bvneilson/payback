a2945ed7140da057138c0401b7540ca78738f67c
unknown <shrthrdude@yahoo.com>
2015-08-03 13:59:32 -0600

    Added resolve button to debts

--- public/ngControllers/dashboardCtrl.js
+++ public/ngControllers/dashboardCtrl.js
@@ -1,61 +1,114 @@
 var app = angular.module("payback");
 
 app.controller("dashboardCtrl", function($scope, user, dashboardService, debtService) {
+    
+    function setResolveBtn(debtDoc){
+        if (debtDoc.status === "Open"){
+            $scope.debtDisplay = true;
+        } else {
+            $scope.debtDisplay = false;
+        }
+    };
+
     $scope.user = user; 
-    console.log(2222, $scope.user)
-    console.log(99999, user)
+    
     $scope.getdata = function(get){
         dashboardService.getTheData(get).then(function(res) {
             console.log(res);
         })
-    }
+    };
+
     $scope.getCurrentUser = function(get){
         dashboardService.getCurrentUser(get).then(function(res) {
             console.log(res);
         })
-    }
+    };
 
     $scope.closeDetails = function(){
         $scope.showDebt = false;
     };
 
+    $scope.closeDebt = function(debtDoc) {
+        debtDoc.status = "Closed";
+        debtService.updateDebt(debtDoc).then(function() {
+            $scope.getDebts();
+            setResolveBtn(debtDoc);
+            console.log("Debt updated.")
+        })
+    };
+
+    $scope.reopenDebt = function(debtDoc) {
+        debtDoc.status = "Open";
+        debtService.updateDebt(debtDoc).then(function() {
+            $scope.getDebts();
+            setResolveBtn(debtDoc);
+            console.log("Debt updated.")
+        })
+    }
+
     $scope.getDebts = function() {
         debtService.getDebts().then(function(data) {
             console.log("debts", data);
             var debtArray = [];
+            var debtTotal = 0;
+            console.log("user.data.id", user.data._id)
             for (var i = 0; i < data.length; i++){
-                if (user.data._id === data[i].userId){
-                    debtArray.push(data[i])
-                }
+                if (user.data._id === data[i].userId  && user.data._id ){
+                    phoneNo = data[i].cellPhone;
+                    data[i].amount = data[i].amount.toFixed(2);
+                    data[i].cellPhone = phoneNo.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
+                    debtArray.push(data[i]);
+                    if (data[i].status === "Open"){
+                        debtTotal += parseInt(data[i].amount);
+                    }
+                
+                };
+
             }
             $scope.debts = debtArray;
+            $scope.debtTotal = debtTotal.toFixed(2);
             
         })
-       // return $scope.debts
     };
     
     $scope.getDebts();
 
     $scope.selected = [];
+    $scope.debtDoc = "";
 
     $scope.gridOptions = { 
         data: 'debts',
+        rowTemplate:'<div style="height: 100%" ng-class="{closedRowColor: row.getProperty(\'status\') === \'Closed\'}"><div ng-style="{ \'cursor\': row.cursor }" ng-repeat="col in renderedColumns" ng-class="col.colIndex()" class="ngCell ">' +
+                           '<div class="ngVerticalBar" ng-style="{height: rowHeight}" ng-class="{ ngVerticalBarVisible: !$last }"></div>' +
+                           '<div ng-cell></div>' +
+                     '</div></div>',
         enableColumnResize: true,
         selectedItems: $scope.selected,
         multiSelect: false,
         afterSelectionChange: function (row, event) {
             $scope.showDebt = false;
-            $scope.debtSelected = $scope.selected[0];
-            if ($scope.debtSelected){
+            $scope.debtDoc = $scope.selected[0];
+
+            if ($scope.debtDoc){
+
               $scope.showDebt = true;
+
+              if ($scope.selected[0].status === "Open"){
+                $scope.debtDisplay = true;
+              } else {
+                $scope.debtDisplay = false;
+              };
+
             };
+            
         },
         height: '200px',
-        sortInfo: {fields: ['Name', 'Amount', 'Description'], directions: ['asc']},
+        sortInfo: {fields: ['Name', 'Amount', 'Description', 'Status'], directions: ['asc']},
         columnDefs: [
             {field: 'fullname', displayName: 'Name'},
             {field: 'amount', displayName: 'Amount'},
-            {field: 'newdescription', displayName: 'Description'}
+            {field: 'newdescription', displayName: 'Description'},
+            {field: 'status', displayName: 'Status'}
         ]
     };
 });
